b6380d945c756e69b7c6e1953b5ae7cc81f7fe90
Bright Webilor <webilorbright@outlook.com>
2022-04-30 09:35:18 +0100

    Sixth Commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,11 @@
         <div class="float-right">
             <button type="button" class="btn ripple btn-success active" data-mdb-toggle="modal" data-mdb-target="#add-user-modal">Add User</button>
         </div>
+
+        <div class="users">
+            <h4>Users</h4>
+            <x-users />
+        </div>
     </div>
     <!--// Entry Point -->
 </x-app-layout>
